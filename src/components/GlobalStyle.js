5ece4c5ef74b22261649977cb5416df7390a1c05
Sujanth <sujanth1987@gmail.com>
2021-05-03 10:50:39 +1000

    remove unwanted files and add fontawesome and google font

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ * {
+     padding: 0;
+     margin: 0;
+     box-sizing: border-box;
+ }
+
+ body {
+    font-family: 'Roboto', sans-serif;
+ }
+`;
+
+export default GlobalStyle;
---
+++ src/components/NotificationHeader.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const NotificationHeader = () => {
+  return (
+    <div>
+      <h1>Notification</h1>
+    </div>
+  );
+};
+
+export default NotificationHeader;
